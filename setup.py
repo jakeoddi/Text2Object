7da1f4755dbdd1e2b26e79bd17928b2dffaf251c
Jake Oddi <jakeoddi@10-20-2-172.dynapool.wireless.nyu.edu>
2021-09-28 02:10:10 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Generate a point-cloud object based on textual user-input.',
+    author='Jake Oddi',
+    license='MIT',
+)
