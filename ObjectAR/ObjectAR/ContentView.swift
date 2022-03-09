3f0f1a3d1b2419cfc106109692b2591a66794f37
Jake Oddi <jakeoddi@gmail.com>
2022-03-09 18:13:49 -0500

    testing AR sphere...

--- ObjectAR/ObjectAR/ContentView.swift
+++ ObjectAR/ObjectAR/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -19,13 +20,28 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+
+        // create sphere for testing
+        let testSphere = MeshResource.generateSphere(radius: 0.05)
+        let testMaterial = SimpleMaterial(color: .blue, roughness: 0, isMetallic: true)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // create model entity
+        let testSphereEntity = ModelEntity(mesh: testSphere, materials: [testMaterial]) // mesh is where obj will be added
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // create anchor
+        let sphereAnchor = AnchorEntity(world: SIMD3(x: 0, y: 0, z: 0))
+        sphereAnchor.addChild(testSphereEntity)
         
+        // add anchor to scene
+        arView.scene.addAnchor(sphereAnchor)
+        
+//
+//        // Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
+//
+//        // Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+//
         return arView
         
     }
